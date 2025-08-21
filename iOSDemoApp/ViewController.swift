cf6f2aabdcf9fda385fe577c66731c4d01af4ebd
89974 <javier.cruzs@bancoazteca.com>
2025-08-21 12:54:15 -0600

    Initial Commit

---
+++ iOSDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSDemoApp
+//
+//  Created by Javier Cruz Santiago on 21/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSDemoAppTests/iOSDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDemoAppTests.swift
+//  iOSDemoAppTests
+//
+//  Created by Javier Cruz Santiago on 21/08/25.
+//
+
+import Testing
+@testable import iOSDemoApp
+
+struct iOSDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
