a954f3c9df4c30c288c36ecafb63b395c51a98f6
Javier Cruz Santiago <jarvis@Javiers-MacBook-Pro-2.local>
2025-09-23 19:57:44 -0600

    [UPDATE] - Added RemoteConfig configuration

---
+++ iOSDemoApp/Firebase/RemoteConfig/RemoteConfigKey.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteConfigKey.swift
+//  iOSDemoApp
+//
+//  Created by Javier Cruz Santiago on 23/09/25.
+//
+
+enum RemoteConfigKey: String {
+    // MARK: - Unsplash Keys
+    /// `String` that contains the Unsplash API Access Key.
+    case unsplash_access_key
+    /// `String` that contains the Unsplash Application Id.
+    case unsplash_application_id
+    /// `String` that contains the Unsplash Secret Key.
+    case unsplash_secret_key
+}
