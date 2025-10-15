07c6e78a772e0040fc64fb402554e51294e43573
89974 <javier.cruzs@bancoazteca.com>
2025-10-14 19:29:16 -0600

    [UPDATE] - Updated ImageDemoView

--- iOSDemoApp/Modules/SwiftUI/UIElements/Image/ImageDemoView.swift
+++ iOSDemoApp/Modules/SwiftUI/UIElements/Image/ImageDemoView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct ImageDemoView: View {
     var body: some View {
-        list
-            .navigationTitle(Text(.init("image")))
-    }
-    
-    var list: some View {
-        List {
+        VStack {
+            Image("Landscape_4")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 200)
+                .border(.blue)
+                .background {
+                    Color(.systemBackground)
+                }
+            
+            Image("Landscape_4")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 400, alignment: .topLeading)
+                .clipped()
             
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Color(.systemGroupedBackground)
+                .ignoresSafeArea()
+        }
+        .navigationTitle(.init("image"))
     }
 }
 
